cb661d96f6cb1a58a71e8decd0eaa6cd99008ce5
nikooo08 <nikocrysostomus_2226240010@mhs.mdp.ac.id>
2024-12-10 18:05:31 +0700

    progres home screen

---
+++ flutter_resepku/lib/data/home_data.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_resepku/models/home.dart';
+
+var homeList = [
+  Home(
+      namaMakanan: 'Nasi Goreng',
+      nama: 'Budi',
+      kategori: 'Main',
+      waktu: '30menit',
+      resep: 'babi potonh',
+      gambar: 'nasii'),
+];
