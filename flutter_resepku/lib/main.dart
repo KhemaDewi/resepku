0f5642681353ba6014ee50d97d6df6840898b50b
nikooo08 <nikocrysostomus_2226240010@mhs.mdp.ac.id>
2024-12-12 22:01:38 +0700

    update

--- flutter_resepku/lib/main.dart
+++ flutter_resepku/lib/main.dart
@@ -32,11 +32,7 @@ class MainApp extends StatelessWidget {
         '/login': (context) => const LoginScreen(),
         '/home': (context) => const HomeScreen(),
         '/register': (context) => const RegisterScreen(),
- home_screen
         '/main': (context) => const MainScreen()
-
-        '/main': (context) => const MainScreen(),
- main
       },
     );
   }
