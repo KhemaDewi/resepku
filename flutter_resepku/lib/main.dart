c69a9ad12a4a7ed2409f8315f0b0ad812d41bd1f
fitriaulianisa <161029948+fitriaulianisa@users.noreply.github.com>
2024-12-12 21:59:58 +0700

    maindart

--- flutter_resepku/lib/main.dart
+++ flutter_resepku/lib/main.dart
@@ -31,12 +31,8 @@ class MainApp extends StatelessWidget {
       routes: {
         '/login': (context) => const LoginScreen(),
         '/home': (context) => const HomeScreen(),
-        '/register': (context) => const RegisterScreen(),
- home_screen
+        '/register': (context) => const RegisterScreen(), 
         '/main': (context) => const MainScreen()
-
-        '/main': (context) => const MainScreen(),
- main
       },
     );
   }
