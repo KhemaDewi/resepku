ce1b8eeb687e2427a1b771c0a3b38f881abfa4da
CalistaVania <161103856+CalistaVania@users.noreply.github.com>
2025-01-05 22:02:34 +0700

    search screen

--- flutter_resepku/lib/screen/search_screen.dart
+++ flutter_resepku/lib/screen/search_screen.dart
@@ -1,16 +1,55 @@
 import 'package:flutter/material.dart';
 
-class SearchScreen extends StatelessWidget {
-  const SearchScreen({super.key});
+class SearchScreen extends StatefulWidget {
+  const SearchScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  final TextEditingController _searchController = TextEditingController();
+  final List<String> _searchHistory = [
+    "ayan betutu",
+    "nasi goreng gila",
+    "pindang patin",
+    "rendang"
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Search'),
+        elevation: 0,
+        backgroundColor: Colors.white,
+        title: TextField(
+          controller: _searchController,
+          decoration: InputDecoration(
+            hintText: "Search...",
+            prefixIcon: const Icon(Icons.search, color: Colors.grey),
+            filled: true,
+            fillColor: Colors.grey[200],
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8),
+              borderSide: BorderSide.none,
+            ),
+          ),
+        ),
       ),
-      body: const Padding(
-          padding: EdgeInsets.all(16), child: Text('Ini adalah halaman search')),
+      body: ListView.builder(
+        itemCount: _searchHistory.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: const Icon(Icons.history, color: Colors.grey),
+            title: Text(_searchHistory[index],
+                style: const TextStyle(fontSize: 16)),
+            onTap: () {
+              // Aksi ketika item diklik
+              print("Selected: ${_searchHistory[index]}");
+            },
+          );
+        },
+      ),
     );
   }
 }
